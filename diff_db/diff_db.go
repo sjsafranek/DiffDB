6e9dd46152460e61f40bf68dbf5a57842ef36466
stefan safranek <stefan@stefan.stefan>
2017-03-23 08:53:45 -0700

    debugging

--- diff_db/diff_db.go
+++ diff_db/diff_db.go
@@ -6,48 +6,51 @@ import (
 
 import "github.com/sjsafranek/SkeletonDB"
 
-func NewDiffDb(db_file string) DiffDb {
-	var diffDb = DiffDb{
-		Filename: db_file,
-		Table:    "DiffData",
-		DB:       skeleton.Database{File: db_file}}
-	diffDb.Init()
-	return diffDb
-}
-
 func (self DiffDb) Init() {
-	if "" == self.Table {
-		self.Table = "DiffData"
-	}
 
+	self.DB = skeleton.Database{File: self.getFile()}
 	self.DB.Init()
 
 	conn := self.DB.Connect()
 	defer conn.Close()
 
-	err := self.DB.CreateTable(conn, self.Table)
+	err := self.DB.CreateTable(conn, self.getTable())
 	if nil != err {
 		panic(err)
 	}
 }
 
+func (self DiffDb) getFile() {
+	if "" == self.File {
+		return = "diff.db"
+	}
+	return self.File
+}
+
+func (self DiffDb) getTable() {
+	if "" == self.Table {
+		return = "DiffData"
+	}
+	return self.Table
+}
+
 func (self *DiffDb) Load(name string) ([]byte, error) {
 	name = strings.ToLower(name)
-	data, err := self.DB.Select(self.Table, name)
+	data, err := self.DB.Select(self.getTable(), name)
 	return data, err
 }
 
 func (self *DiffDb) Save(name string, data []byte) error {
-	err := self.DB.Insert(self.Table, name, data)
+	err := self.DB.Insert(self.getTable(), name, data)
 	return err
 }
 
 func (self *DiffDb) Remove(name string) error {
-	err := self.DB.Remove(name, self.Table)
+	err := self.DB.Remove(name, self.getTable())
 	return err
 }
 
 func (self *DiffDb) SelectAll() ([]string, error) {
-	data, err := self.DB.SelectAll(self.Table)
+	data, err := self.DB.SelectAll(self.getTable())
 	return data, err
 }
